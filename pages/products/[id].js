d1ab7062dd5b536c72872f65d54a142e6339651d
sockry12 <971008.ed@gmail.com>
2021-01-13 14:47:51 -0600

    beta

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -15,7 +15,7 @@ import { useContext, useState } from 'react';
 import { useStyles } from '../../utils/styles';
 import { Store } from '../../components/Store';
 import { CART_RETRIEVE_REQUEST, CART_RETRIEVE_SUCCESS } from '../../utils/constants';
-import Router from 'next/dist/next-server/lib/router/router';
+import Router from 'next/router';
 
 export default function Product(props) {
   const{product} = props;
@@ -34,11 +34,11 @@ export default function Product(props) {
               quantity: quantity,
           });
           dispatch({ type: CART_RETRIEVE_REQUEST, payload: cartData.cart });
-          //Router.push('/cart');
+          Router.push('/cart');
       } else {
           const cartData = await commerce.cart.add(product.id, quantity);
           dispatch({ type: CART_RETRIEVE_SUCCESS, payload: cartData.cart});
-          //Router.push('/cart');
+          Router.push('/cart');
       }
   };
   return (
